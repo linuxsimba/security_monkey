6d43d278a410b2846b15adac96389a826849c23b
vagrant <vagrant@securitymonkey>
2016-11-17 15:16:14 +0000

    update docker files.

--- docker.linuxsimba/docker-entrypoint.sh
+++ docker.linuxsimba/docker-entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 
 cd /usr/local/src/security_monkey
-export SECURITY_MONKEY_SETTINGS=/usr/local/src/security_monkey/env-config/config-deploy.py
-python manage.py db upgrade
+
+SECURITY_MONKEY_SETTINGS=/usr/local/src/security_monkey/env-config/config-deploy.py python manage.py db upgrade
+chown -R www-data /var/log/security_monkey
+
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
